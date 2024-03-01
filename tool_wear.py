6d0c0cb51048fd9855dc7d722541411ca583dda3
Asogwa Chukwuemeka <101509899+Mekazstan@users.noreply.github.com>
2024-03-01 05:55:46 +0100

    Update tool_wear.py

--- tool_wear.py
+++ tool_wear.py
@@ -112,6 +112,7 @@ y = df['Target']
 # -----> Training & Building the model
 
 # Spliting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(X_train, y_train)
 
 # Initializing the Decision Tree Classifier within a pipeline
